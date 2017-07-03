6cdfe10c5e44ad59b3b8d421e64d7f452bbd85bf
rlaeogus <rlaeogus456@hanmail.net>
2017-07-03 14:26:30 +0900

    20170703

--- data/dbconfig.php
+++ data/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined('_GNUBOARD_')) exit;
 define('G5_MYSQL_HOST', 'localhost');
-define('G5_MYSQL_USER', 'leemited');
-define('G5_MYSQL_PASSWORD', 'flug0258');
-define('G5_MYSQL_DB', 'leemited');
+define('G5_MYSQL_USER', 'sangho6777');
+define('G5_MYSQL_PASSWORD', 's97556417');
+define('G5_MYSQL_DB', 'sangho6777');
 define('G5_MYSQL_SET_MODE', false);
 
 define('G5_TABLE_PREFIX', 'g5_');
